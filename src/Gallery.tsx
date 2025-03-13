47676299b645fa928d89b13828acfa4a80e69f67
bastndev <gohit24@gmail.com>
2025-03-12 21:12:07 -0500

    🏗️ [ADD] nice scroll bar

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,13 +1,24 @@
-import "./Index.css"
+import "./Index.css";
 
 import { useEffect, useRef } from "@lynx-js/react";
+import type { ScrollEvent } from "@lynx-js/types";
 import type { NodesRef } from "@lynx-js/types";
 import LikeImageCard from "./Components/ImageCard.jsx";
 import type { Picture } from "./Pictures/furnitures/furnituresPictures.jsx";
 import { calculateEstimatedSize } from "./utils.jsx";
+import { NiceScrollbar, type NiceScrollbarRef } from "./Components/NiceScrollBar.jsx";
 
 export const Gallery = (props: { pictureData: Picture[] }) => {
   const { pictureData } = props;
+  const scrollbarRef = useRef<NiceScrollbarRef>(null);
+
+  const onScroll = (event: ScrollEvent) => {
+    scrollbarRef.current?.adjustScrollbar(
+      event.detail.scrollTop,
+      event.detail.scrollHeight,
+    );
+  };
+
   const galleryRef = useRef<NodesRef>(null);
 
   useEffect(() => {
@@ -24,6 +35,7 @@ export const Gallery = (props: { pictureData: Picture[] }) => {
 
   return (
     <view className="gallery-wrapper">
+      <NiceScrollbar ref={scrollbarRef} />
       <list
         ref={galleryRef}
         className="list"
@@ -31,10 +43,14 @@ export const Gallery = (props: { pictureData: Picture[] }) => {
         column-count={2}
         scroll-orientation="vertical"
         custom-list-name="list-container"
+        bindscroll={onScroll}
       >
         {pictureData.map((picture: Picture, index: number) => (
           <list-item
-            estimated-main-axis-size-px={calculateEstimatedSize(picture.width, picture.height)}
+            estimated-main-axis-size-px={calculateEstimatedSize(
+              picture.width,
+              picture.height,
+            )}
             item-key={"" + index}
             key={"" + index}
           >
